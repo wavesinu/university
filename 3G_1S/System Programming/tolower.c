723ebcf249b8be6cc22baeab0ce0a8286283061b
wavesinu <wavesinu@gmail.com>
2023-04-10 13:24:29 +0900

    Add: tolower.c 코드 구현

--- 3G_1S/System Programming/tolower.c
+++ 3G_1S/System Programming/tolower.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
-int main(){
-	
-	return 0;
+int main(int argc, char *argv[]) {
+    // open file
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+		printf("Cannot open file.");
+        return 1;
+    }
+
+    // read file
+    int c;
+    while ((c = fgetc(fp)) != EOF) {
+        if (isupper(c)) {
+            putchar(tolower(c));
+        } else {
+            putchar(c);
+        }
+    }
+
+    // close file
+    fclose(fp);
+
+    return 0;
 }
