6fc5306a18c25c184430ba27c99705ad4914f348
ISU <wavesinu@gmail.com>
2023-06-03 17:05:42 +0900

    Fix: 두 클라이언트 간의 메세지가 제대로 전달되지 않는 오류 수정

--- 3G-1S/system_programming/message-queue-assginment/server.c
+++ 3G-1S/system_programming/message-queue-assginment/server.c
@@ -1,66 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
 
 #define SERVER_KEY 0x2021033161
 #define CLIENT1_KEY 0x2021033162
 #define CLIENT2_KEY 0x2021033163
-
 #define CLIENT1_MTYPE 100
 #define CLIENT2_MTYPE 200
+#define MAX_TEXT_SIZE 100
 
-struct msgbuf
+struct message
 {
     long mtype;
-    char mtext[200];
+    char mtext[MAX_TEXT_SIZE];
 };
 
 int main()
 {
-    int server_id, client1_id, client2_id;
-    struct msgbuf buf;
-
-    int quit_cnt = 0;
-
-    server_id = msgget(SERVER_KEY, 0666 | IPC_CREAT);
-    client1_id = msgget(CLIENT1_KEY, 0666 | IPC_CREAT);
-    client2_id = msgget(CLIENT2_KEY, 0666 | IPC_CREAT);
-
-    if (server_id == -1 || client1_id == -1 || client2_id == -1)
+    int server_msgid = msgget(SERVER_KEY, IPC_CREAT | 0666);
+    if (server_msgid == -1)
     {
-        perror("msgget failed");
-        exit(EXIT_FAILURE);
+        perror("msgget");
+        exit(1);
     }
+    printf("Message queue %lx created\n", SERVER_KEY);
 
-    printf("message queue 0x%08lx 생성\n", SERVER_KEY);
+    struct message msgbuf;
+    int quit_count = 0;
 
-    while (quit_cnt < 2)
+    while (1)
     {
-        msgrcv(server_id, &buf, sizeof(buf.mtext), 0, 0);
+        if (msgrcv(server_msgid, &msgbuf, sizeof(struct msg), 0, 0) == -1)
+        {
+            perror("msgrcv");
+            exit(1);
+        }
+        if (strcmp(msgbuf.mtext, "quit") == 0)
+        {
+            quit_count++;
+            if (quit_count == 2)
+                break;
+        }
+        int target_msgid;
+        if (msgbuf.mtype == CLIENT1_MTYPE)
+        {
+            msgbuf.mtype = CLIENT2_MTYPE;
+            target_msgid = msgget(CLIENT2_KEY, 0666);
+            if (target_msgid == -1)
+            {
+                perror("msgget");
+                exit(1);
+            }
+            printf("recv from client1 : %s\n", msgbuf.mtext);
+            printf("send to client2 : %s\n", msgbuf.mtext);
+        }
+        else
+        {
+            msgbuf.mtype = CLIENT1_MTYPE;
+            target_msgid = msgget(CLIENT1_KEY, 0666);
+            if (target_msgid == -1)
+            {
+                perror("msgget");
+                exit(1);
+            }
+            printf("recv from client2 : %s\n", msgbuf.mtext);
+            printf("send to client1 : %s\n", msgbuf.mtext);
+        }
+        int status = msgsnd(target_msgid, &msgbuf, sizeof(struct msg), 0);
+        if (status == -1)
+        {
+            perror("msgsnd");
+            exit(1);
+        }
 
-        if (buf.mtype == CLIENT1_MTYPE)
-        {
-            printf("recv from client1: %s\n", buf.mtext);
-            msgsnd(client2_id, &buf, sizeof(buf.mtext), 0);
-            printf("send to client2: %s\n", buf.mtext);
-        }
-        else if (buf.mtype == CLIENT2_MTYPE)
-        {
-            printf("recv from client2: %s\n", buf.mtext);
-            msgsnd(client1_id, &buf, sizeof(buf.mtext), 0);
-            printf("send to client1: %s\n", buf.mtext);
-        }
-
-        if (strcmp(buf.mtext, "quit") == 0)
-        {
-            quit_cnt++;
-        }
+        if (msgbuf.mtext == CLIENT1_MTYPE)
+            msgbuf.mtype = CLIENT2_MTYPE;
+        else
+            msgbuf.mtype = CLIENT1_MTYPE;
     }
-
-    msgctl(server_id, IPC_RMID, NULL);
-    printf("server message queue removed\n");
-
+    if (msgctl(server_msgid, IPC_RMID, NULL) == -1)
+    {
+        perror("msgctl");
+        exit(1);
+    }
+    printf("Server message queue removed\n");
     return 0;
 }
