a62c131499c38322243484459dd196af5df7e2da
ISU <wavesinu@gmail.com>
2023-06-04 20:48:43 +0900

    CMake 설정

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wavesinu/Documents/University
+# Build directory: /Users/wavesinu/Documents/University/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
